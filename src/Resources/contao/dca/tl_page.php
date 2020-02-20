9e1b7ba37ae745e9df8064bd6360ef1888643f01
Mathias <arzberger@pdir.de>
2020-02-20 12:52:01 +0100

    add page ids in page structure (backend)

---
+++ src/Resources/contao/dca/tl_page.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_page']['list']['label']['fields'][] = 'id';
+$GLOBALS['TL_DCA']['tl_page']['list']['label']['format'] = '%s <span style="color:#999999;padding-left:3px;font-weight:normal;">[ID: %s]</span>';
